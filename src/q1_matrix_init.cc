b3070dd0cd62d1c76391cd782d3d13972fd2c1a7
Danil <daniil.ibragimov.10@mail.ru>
2025-10-02 21:43:08 +0300

    yo

--- src/q1_matrix_init.cc
+++ src/q1_matrix_init.cc
@@ -4,18 +4,16 @@ int main() {
   std::cout << "Введите количество строк и столбцов: " << std::endl;
   int n1, n2;
   std::cin >> n1 >> n2;
-  
+
   int arr[n1][n2];
   std::cout << "Введите элементы: " << std::endl;
   for (int i = 0; i < n1; i++) {
-  
     for (int j = 0; j < n2; ++j) {
       std::cin >> arr[i][j];
     }
   }
   std::cout << "Матрица по вашим параметрам готова:" << std::endl;
   for (int i = 0; i < n1; i++) {
-  
     for (int j = 0; j < n2; ++j) {
       std::cout << arr[i][j] << ' ';
     }
